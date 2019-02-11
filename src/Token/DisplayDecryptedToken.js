d57e89172ab0bd763c7a491fa975634f3e08c86d
qed0711 <qdizon@gmail.com>
2019-02-11 07:19:45 -0500

    verified token displays date correctly

--- src/Token/DisplayDecryptedToken.js
+++ src/Token/DisplayDecryptedToken.js
@@ -5,16 +5,40 @@ import cryptr from '../js/encryption';
 class DisplayDecryptedToken extends Component {
 
     decryptToken(token){
+        try{
+            cryptr.decrypt(token)
+        } catch(error){
+            return null
+        }
         return cryptr.decrypt(token)
+    }
+
+    parseDate(date){
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        date = new Date(date)
+        return `${days[date.getDay()]}, ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}, at ${date.getHours()}:${date.getMinutes()} `
     }
 
     render(){
         let decrypted = JSON.parse(this.decryptToken(this.props.token))
         return(
             <div id='decrypted-token'>
-                <h4>Name: {decrypted.studentName}</h4>
-                <h4>Date: {decrypted.date}</h4>
-                <a href={decrypted.answer} target="_blank"><h4>View Score</h4></a>
+                {
+                    decrypted
+                    &&
+                    <div>
+                        <h3>Verified</h3>
+                        <h4>Name: {decrypted.studentName}</h4>
+                        <h4>Completed On: {this.parseDate(decrypted.date)}</h4>
+                        <a href={decrypted.answer} target="_blank"><h4>View Score</h4></a>
+                    </div>
+                }               
+
+                {
+                    !decrypted
+                    &&
+                    <h3>The token you have entered is invalid</h3>
+                }
             </div>
         )
     }
