812284365c183434f115d8547fbce1bed95203e8
qed0711 <qdizon@gmail.com>
2019-02-12 06:20:20 -0500

    the home page and assignment page may share the same path with search string params

--- src/App.js
+++ src/App.js
@@ -11,26 +11,45 @@ import TokenChecker from './Token/TokenChecker';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      path : "/"
+    }
+
+    this.setPath = this.setPath.bind(this);
+  }
+
+  setPath(path){
+    this.setState({path});
+    this.forceUpdate();
+
+  }
+
   render() {
     const search = new window.URLSearchParams(window.location.search);
+    let path = window.location.href.split("/")
+    path = path[path.length - 1];
     return (
       <BrowserRouter>
         <div className="App">
-          <Navigation />
-          {/* {search.get("path") === null && <InfoContainer />} */}
-          {/* {search.get("path") === "new" && <NewAssignmentContainer />}
-          {search.get("path") === "token-checker" && <TokenChecker />} */}
+          <Navigation setPath={this.setPath} />
 
-          {/* {search.get("path") === "assignment" && <AssignmentContainer />} */}
           {
-            search.get("assignment")
+            (this.state.path === "/" && search.get("assignment"))
             &&
             <AssignmentContainer />
           }
-
-          <Route path="/info" component={InfoContainer} />
+          {
+            (this.state.path === "/" && !search.get("assignment"))
+            &&
+            <Route path={"/"} exact component={InfoContainer} />
+          }
           <Route path="/new" exact component={NewAssignmentContainer} />
           <Route path="/token-checker" exact component={TokenChecker} />
+
+          
 
         </div>
       </BrowserRouter>
--- src/Banner/Navigation.js
+++ src/Banner/Navigation.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 import {Link} from 'react-router-dom';
 
-const Navigation = () => {
+const Navigation = ({setPath}) => {
+
+   const handleClick = (path) => {
+        return (e) => {
+            setPath(path);
+        }
+    }
 
     return(
         <nav id="main-nav-banner">
@@ -10,9 +16,9 @@ const Navigation = () => {
             <a className="main-nav-link" href="/?path=new"><h3>New Assignment</h3></a>
             <a className="main-nav-link" href="/?path=token-checker"><h3>Check Token</h3></a> */}
 
-            <Link to="/info">Information & Tutorials</Link>
-            <Link to="/new">New Assignment</Link>
-            <Link to="/token-checker">Check Token</Link>
+            <Link to="/" onClick={handleClick("/")}>Information & Tutorials</Link>
+            <Link to="/new" onClick={handleClick("/new")}>New Assignment</Link>
+            <Link to="/token-checker" onClick={handleClick("/token-checker")}>Check Token</Link>
         </nav>
     )
 
