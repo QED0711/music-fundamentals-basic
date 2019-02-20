36911e9befa039ce2c491f4cc6748dca39f82ced
qed0711 <qdizon@gmail.com>
2019-02-20 09:03:32 -0500

    info content

--- src/Information/content/instructorContent.js
+++ src/Information/content/instructorContent.js
@@ -1,5 +1,10 @@
+import {paragraph, sectionHeading, orderedList} from './contentTags'
+
 const instructorContent = {
-    title: "Instructor Tutorials"
+    title: "Instructor Tutorials",
+    data: [
+        sectionHeading("Getting Started"),
+    ]
 
 }
 
