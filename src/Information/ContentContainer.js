2bb34c0713a6e5c4cbd4e1cd05f4e4a08537049d
qed0711 <qdizon@gmail.com>
2019-03-02 21:21:38 -0500

    color scheme redesign

--- src/Information/ContentContainer.js
+++ src/Information/ContentContainer.js
@@ -2,10 +2,6 @@ import React, {Component} from "react";
 
 class ContentContainer extends Component {
 
-    renderContent(data){
-
-    }
-
     render(){
         const {content} = this.props
         return(
